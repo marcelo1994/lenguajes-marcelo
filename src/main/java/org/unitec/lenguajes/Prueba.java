99f4da657eb5388308e5363faba8a76c2919120a
T-107 <T-107@PC260>
2016-11-14 19:01:11 -0600

    Mi primer proyecto subido a github

--- src/main/java/org/unitec/lenguajes/Prueba.java
+++ src/main/java/org/unitec/lenguajes/Prueba.java
@@ -7,7 +7,7 @@ public class Prueba {
     
     public static void main(String[] args) {
         
-        System.out.println("Probando Git Hub... ");
+        System.out.println("Probando Git Hub...  ");
         
         
     }
