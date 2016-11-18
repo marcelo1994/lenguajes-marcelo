e8e32b36ef8890f64b3f61c6b9f00ab0926d3d5a
T-107 <T-107@PC260>
2016-11-17 19:11:06 -0600

    Mi primer pushero

--- src/main/java/org/unitec/lenguajes/Prueba.java
+++ src/main/java/org/unitec/lenguajes/Prueba.java
@@ -1,14 +1,54 @@
 
 package org.unitec.lenguajes;
 
-
-public class Prueba {
+/*Segunda forma de crear Threads*/
+public class Prueba implements Runnable{
     
     
     public static void main(String[] args) {
         
         System.out.println("Probando Git Hub Ahoraa ...  ");
+        Runnable r=new Prueba();
+        Thread t1=new Thread(r);
+        t1.setName("Juan");
+        Thread t2=new Thread(r);
+        t2.setName("Ana");
+      //  t1.start();
+      //  t2.start();
+      
+      //Tercera manera de crear Threads que viene en el examen.
+      //Usando clases anonimas.
+      
+      Thread t3=new Thread(new Runnable(){
+            @Override
+            public void run() {
+              
+      System.out.println("Soy un background Thread..."); 
+                
+            }
+      });
+      t3.start();
+      
         
+    }
+
+    @Override
+    public void run() {
+        /*
+        
+         System.out.println("Soy un Thread...y me llamo "+ Thread.currentThread().getName());
+         
+        if((Thread.currentThread().getName()).equals("Ana")){
+            System.out.println("Soy mujer...");
+            
+        }else{
+        System.out.println("Soy hombre...");
+    }
+        
+        
+       
+        
+      */  
         
     }
     
